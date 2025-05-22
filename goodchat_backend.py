7f5e08e667b201ffbb1ed662bb553de1d5593676
Swarnavo-02 <0swarnavodas@gmail.com>
2025-05-23 00:06:27 +0530

    goodchat_backend.py

--- goodchat_backend.py
+++ goodchat_backend.py
@@ -1141,4 +1141,4 @@ def handle_disconnect():
     print("User disconnected")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000) 
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
